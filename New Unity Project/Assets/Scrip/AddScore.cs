80a814b4d7014b5f435f46587ee5c1337ad7db79
arnoldjin0123 <71679939+arnoldjin0123@users.noreply.github.com>
2020-12-22 11:16:11 +0800

    City set
    Score scrip set, reset scrip set,
    city model set

---
+++ New Unity Project/Assets/Scrip/AddScore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour
+{
+    private int ScoreVal=0;
+    public Text RScoreBoard;
+    public Text LScoreBoard;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag=="Ball3")
+        {
+            ScoreVal =ScoreVal+3;
+        }
+        if (col.tag == "Ball2")
+        {
+            ScoreVal = ScoreVal + 2;
+        }
+    }
+
+    void Start()
+    {
+        RScoreBoard.text = "Your score:/n" + ScoreVal;
+        RScoreBoard.text = LScoreBoard.text;
+    }
+}
