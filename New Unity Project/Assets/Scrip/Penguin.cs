c4d88d99df0e8346c467e0e22c2e53be03144405
arnoldjin0123 <71679939+arnoldjin0123@users.noreply.github.com>
2021-01-05 16:14:11 +0800

    Finish main scence
    Only need to buid Title scence and finish

--- New Unity Project/Assets/Scrip/Penguin.cs
+++ New Unity Project/Assets/Scrip/Penguin.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Penguin : MonoBehaviour
 {
+    //追蹤可調參數
     [Header("MovingSpeed"), Range(0, 50)]
     public float speed = 3f;
     [Header("StopDistance"), Range(0, 3)]
     public float StopDistance = 2.5f;
-
+    //指定欄位
     private Transform ball;
     private Transform player;
     private NavMeshAgent nav;
     private Animator PenAni;
-
+    //切換目標所需元件
     public GameObject PetBall;
     public GameObject Bitball;
-    private bool ballDissplay=false; //True=ball can see, False=ball can't see
-    private bool target=false; //False=target is ball, true=target is player
+    public GameObject PlayerPositive;
+    private bool PetballDissplay=true;  //True=Petball can see, False=Petball can't see
+    private int TargetSwitch = 0;
+    //計時器元件
+    float Timer_f = 0f;
+
 
     private void Awake()
     {
@@ -33,33 +39,75 @@ public class Penguin : MonoBehaviour
 
     void Update()
     {
+        timer();
         Track();
+        PetballEvent();
+        TargetChange();
     }
 
     private void Track()
     {
-        nav.SetDestination(ball.position);
-        Debug.Log("Penguin and ball's distance" + nav.remainingDistance);
-        if (nav.remainingDistance<=StopDistance && ballDissplay==true)
-        {
-            PetBall.SetActive(false);
-            ballDissplay = false;
-            Bitball.SetActive(true);
-            nav.SetDestination(player.position);
-        }
-        if (nav.remainingDistance <= StopDistance && ballDissplay == false)
-        {
-            PetBall.SetActive(true);//將球的位子更改到企鵝前方並顯示出來
-            ballDissplay = true;
-        }
+        Debug.Log("Penguin and Targit distance" + nav.remainingDistance);
         PenAni.SetBool("Walk", nav.remainingDistance > StopDistance);
-    }
-    // Start is called before the first frame update
-    void Start()
+    }//Debug企鵝和目標距離, 依距離判斷走路動畫
+
+    private void PetballEvent()
     {
-        
+        if (PetballDissplay == true) 
+        {
+            Bitball.SetActive(false);
+            PetBall.SetActive(true);
+            nav.SetDestination(ball.position);
+            Debug.Log("Ball is the targit");
+        }//嘴上的球不顯示, 可丟的球顯示, 追蹤目標為可丟的球
+
+        if (PetballDissplay == false) 
+        {
+            Bitball.SetActive(true);
+            PetBall.SetActive(false);
+            nav.SetDestination(player.position);
+            Debug.Log("player is the targit");
+        }//嘴上的球顯示, 可丟的球不顯示,追蹤目標為可丟的球
+    } //可丟的球顯示與不顯示各自的事件
+
+    private void timer()
+    {
+            Timer_f += Time.deltaTime;
+            Debug.Log("Time" + Timer_f);
     }
 
-    // Update is called once per frame
+    private void TargetChange()
+    {
+        if (nav.remainingDistance>2 && TargetSwitch == 0)
+        {
+            TargetSwitch++;
+            Debug.Log("TargetSwitch:"+ TargetSwitch);
+        }
+        if (nav.remainingDistance<2 && TargetSwitch==1)
+        {
+            PetballDissplay = false;
+            TargetSwitch++;
+            Debug.Log("TargetSwitch:" + TargetSwitch);
+            Timer_f = 0f;
+        }
+        if (nav.remainingDistance < 2 && TargetSwitch == 2 &&Timer_f>2f)
+        {
+            TargetSwitch = 0;
+            PetballDissplay = true;
+            Vector3 move = PlayerPositive.transform.position;
+            move = new Vector3(move.x, move.y, move.z + 0.5f);
+            PetBall.transform.position = move;
+            PetballDissplay = true;
+            Debug.Log("TargetSwitch:" + TargetSwitch);
+        }
+    }
+
+    public void PetReset()
+    {
+        TargetSwitch = 0;
+        Timer_f = 0f;
+        PetballDissplay = true;
+        Debug.Log("ResetPet");
+    }
 
 }
---
+++ New Unity Project/Assets/Scrip/UIButtonEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonEvent : MonoBehaviour
+{
+    public GameObject pet;
+    public GameObject petball;
+    public void ResetPetAndBall()
+        {
+        pet.transform.position = new Vector3(-0.2862205f, 0.9522928f, 18.99f);
+        pet.transform.Rotate(0, 180f, 0f);
+        petball.transform.position = new Vector3(-0.3145199f, 1.88f, 17.22f);
+        }
+}
