4c96a2a299c4a04a86338f9b240497f3d144d87a
arnoldjin0123 <71679939+arnoldjin0123@users.noreply.github.com>
2021-01-06 20:48:33 +0800

    Project Finish
    Title:Game start, Game exit, Reset ball
    BaseLevel:Add score, Reset level, Exit level,  Penguin, Reset pengine ball

---
+++ New Unity Project/Assets/Scrip/CallStartBallBack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallStartBallBack : MonoBehaviour
+{
+    public GameObject StartBall;
+
+    public void CallBallBack()
+    {
+        StartBall.transform.position = new Vector3(6f,63.2f,11f);
+    }
+}
---
+++ New Unity Project/Assets/Scrip/IntoBaseStage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntoBaseStage : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Ball")
+        {
+            SceneManager.LoadScene("BaseLevel");
+            Debug.Log("GameStart");
+        }
+    }
+
+}
